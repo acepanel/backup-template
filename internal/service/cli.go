83a87302f3e80102bd55911071e06a48af1c5ef2
耗子 <haozi@loli.email>
2025-12-31 21:55:56 +0800

    fix: lint

--- internal/service/cli.go
+++ internal/service/cli.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"path/filepath"
 
 	"github.com/knadh/koanf/v2"
@@ -36,7 +36,7 @@ func (s *CliService) Check(ctx context.Context, cmd *cli.Command) error {
 func (s *CliService) Mkdir(ctx context.Context, cmd *cli.Command) error {
 	for _, dir := range cmd.Args().Slice() {
 		if dir == "" {
-			return fmt.Errorf(s.t.Get("directory path is required"))
+			return errors.New(s.t.Get("directory path is required"))
 		}
 		// TODO
 	}
@@ -49,7 +49,7 @@ func (s *CliService) Mkdir(ctx context.Context, cmd *cli.Command) error {
 func (s *CliService) Deldir(ctx context.Context, cmd *cli.Command) error {
 	for _, dir := range cmd.Args().Slice() {
 		if dir == "" {
-			return fmt.Errorf(s.t.Get("directory path is required"))
+			return errors.New(s.t.Get("directory path is required"))
 		}
 		// TODO
 	}
@@ -63,7 +63,7 @@ func (s *CliService) Put(ctx context.Context, cmd *cli.Command) error {
 	localPath := cmd.Args().Get(0)
 	remotePath := cmd.Args().Get(1)
 	if localPath == "" || remotePath == "" {
-		return fmt.Errorf(s.t.Get("local and remote file paths are required"))
+		return errors.New(s.t.Get("local and remote file paths are required"))
 	}
 
 	// TODO
@@ -76,7 +76,7 @@ func (s *CliService) Put(ctx context.Context, cmd *cli.Command) error {
 func (s *CliService) Del(ctx context.Context, cmd *cli.Command) error {
 	for _, file := range cmd.Args().Slice() {
 		if file == "" {
-			return fmt.Errorf(s.t.Get("file path is required"))
+			return errors.New(s.t.Get("file path is required"))
 		}
 		// TODO
 	}
